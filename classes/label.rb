8a930f5d4e9f37f46642a10adfbeb8988f0013c6
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-07-05 20:29:02 +0430

    Initial class code

--- classes/label.rb
+++ classes/label.rb
@@ -1,7 +1,14 @@
 require_relative './../item'
-classs Label
-
-def add_item(item)
-
-end    
+class Label
+    attr_accessor :title, :color, :item
+    def initialize(id, title, color, items)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+    def add_item(item: item)
+        @item = item
+        @items << self
+    end
 end
