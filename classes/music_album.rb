d46fc6d0fd7e33d657b2a7b7b18d484d2560a681
amrendrakind <vision2010@gmail.com>
2022-07-05 21:42:48 +0530

    Update: music_album test

---
+++ classes/music_album.rb
@@ -0,0 +1,5 @@
+class MusicAlbum
+    def initialize
+        
+    end
+end
---
+++ spec/music_album_spec.rb
@@ -0,0 +1,11 @@
+require_relative './../music_album'
+
+describe 'music_album TDD' do
+  context 'can be archived' do
+    it 'response' do
+      music_album=MusicAlbum.new
+      response =music_album.can_be_archived?()
+      expect(response).to eq(true)
+    end
+end
+end
