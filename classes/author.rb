b2bd44d92753f788db785d5b059b0731400a8514
amrendrakind <vision2010@gmail.com>
2022-07-06 21:27:54 +0530

    Linter error fixed

--- classes/author.rb
+++ classes/author.rb
@@ -1,9 +1,6 @@
 require_relative './item'
 
 class Author
-  def initialize
-    
-  end  
   def add_item(_item)
     true
   end
--- classes/item.rb
+++ classes/item.rb
@@ -4,8 +4,8 @@ require 'date'
 class Item < JSONable
   attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
-  def initialize(publish_date)
-     @id = Random.rand(1..1000)
+  def initialize(publish_date) # rubocop:disable Lint/MissingSuper
+    @id = Random.rand(1..1000)
     @publish_date = Date.parse(publish_date)
     @archived = false
   end
