f3fdb219d3b72cb85889d88c5887325fe6ee85d6
amrendrakind <vision2010@gmail.com>
2022-07-07 01:13:43 +0530

    Update: Author and game class

--- classes/author.rb
+++ classes/author.rb
@@ -1,7 +1,16 @@
 require_relative './item'
 
 class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :items, :id
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @id = Random.rand(1..1000)
+    @items = []
+  end
   def add_item(_item)
-    true
+    @items << _item
+    _item.author = self
   end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -1,9 +1,15 @@
-class Game
-  def initialize
-    puts 'test'
+require 'date'
+require_relative './item'
+
+class Game < item
+  attr_accessor :multiplayer, :last_played_at
+  def initialize(multiplayer, last_played_at)
+    super(multiplayer)
+    @multiplayer = multiplayer
+    @last_played_at = Date.new(last_played_at.to_i)
   end
 
   def can_be_archived?
-    true
+    return true if super and (Date.today.year - @last_played_at.year > 2)
   end
 end
