c75b96b4a139c979b5eddcc2b903964df09c8890
amrendrakind <vision2010@gmail.com>
2022-07-07 04:25:38 +0530

    Update: list and add of Author

--- app.rb
+++ app.rb
@@ -1,17 +1,20 @@
 require './base_file'
 require './classes/music_album'
+require './classes/game'
 require 'json'
 
 class App
   def initialize
     @book_file = BaseFile.new('./json/book.json')
     @music_album_file = BaseFile.new('./json/music_album.json')
-    @game_file = BaseFile.new('./json/game.json')
     @genre_file = BaseFile.new('./json/genre.json')
+    @game_file = BaseFile.new('./json/game.json')
+    @author_file = BaseFile.new('./json/author.json')
   end
 
   def list_books
     puts 'List of books'
+
   end
 
   def list_music_albums
@@ -25,7 +28,13 @@ class App
   end
 
   def list_games
-    puts 'List of games'
+    data = @game_file.read_all_records
+    if data.empty?
+      puts("No game found. \n")
+    else
+      puts("List of game: \n")
+      puts(data)
+    end
   end
 
   def list_genres
@@ -44,6 +53,13 @@ class App
 
   def list_authors
     puts 'list of Authors'
+    data = @author_file.read_all_records
+    if data.empty?
+      puts("No Authors found. \n")
+    else
+      puts("List of Authors: \n")
+      puts(data)
+    end
   end
 
   def list_sources
@@ -68,6 +84,27 @@ class App
   end
 
   def add_game
-    puts 'Add games'
+    puts "Adding New  Games\n"
+    print('Enter Author first name: ')
+    first_name = gets.chomp.to_s
+    print('Enter Author last name: ')
+    last_name = gets.chomp.to_s
+    author = Author.new(first_name, last_name)
+    json_author = author.to_json
+    json_author = json_author.to_s.gsub('@', '')
+    @author_file.write_object(json_author)
+    puts "Author created"
+    puts('Enter game published date (YYYY-MM-DD): ')
+    published_date = gets.chomp.to_s
+    puts('Is this game multiplayer? [Y/N]: ')
+    response = gets.chomp
+    multiplayer = response.downcase == 'y'
+    puts('Date on Game last played at (YYYY-MM-DD): ')
+    last_played_at = gets.chomp.to_s
+    game = Game.new(published_date, multiplayer,last_played_at)
+    json_game = game.to_json
+    json_game = json_game.to_s.gsub('@', '')
+    @game_file.write_object(json_game)
+    puts("Created game id: #{game.id} published: #{game.publish_date}")
   end
 end
