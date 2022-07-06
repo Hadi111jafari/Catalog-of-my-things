5e36c55b08739283674c9b120f5a5c53da1e5f66
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-07-06 11:18:49 +0430

    Implement methods and setup properties for item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,9 +1,38 @@
+require 'date'
+
 class Item
-    def initialize()
-        
-    end
-    attr_reader: published_date
-    def can_be_archived?()
-        @published_date
-    end
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.parse(publish_date)
+    @archived = false
+  end
+
+  def can_be_archived?
+    Date.today.year - @publish_date.year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+  def add_source(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
 end    
