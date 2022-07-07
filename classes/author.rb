c9364f87274d1e254fbaacd362e84818feb3314e
amrendrakind <vision2010@gmail.com>
2022-07-07 05:42:39 +0530

    Linter error fixed

--- classes/author.rb
+++ classes/author.rb
@@ -5,8 +5,7 @@ class Author < JSONable
   attr_accessor :first_name, :last_name
   attr_reader :items, :id
 
-  def initialize(first_name, last_name)
-    super
+  def initialize(first_name, last_name) # rubocop:disable Lint/MissingSuper
     @first_name = first_name
     @last_name = last_name
     @id = Random.rand(1..1000)
